c02bc77aa90c68b90ab27cb79192b3c5ab5e6ced
dianacastano <diamarcast1803@gmail.com>
2022-08-22 14:46:33 +0200

    cambios

--- MiniProyecto/routes/impartidas.routers.js
+++
@@ -1,7 +0,0 @@
-const {Router} = require ("express")
-const router = Router();
-const impartidasCtrl = require ("../../controller/impartidas.contoller")
-
-router.get ("/", impartidasCtrl.getImpartidas);
-
-module.exports = router;
